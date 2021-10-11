d371ba0db8c2a7f02a1ce50c5d792d898e4b0202
Alexander Egorenkov <egorenar@linux.ibm.com>
2021-10-11 16:23:31 +0200

    zdump: Cleanup zgetdump.h C header
    Separate the definition of a struct from the declaration of a global
    variable of the type. To improve readability.
    
    Signed-off-by: Alexander Egorenkov <egorenar@linux.ibm.com>
    Signed-off-by: Jan Höppner <hoeppner@linux.ibm.com>

--- zdump/zgetdump.h
+++ zdump/zgetdump.h
@@ -1,8 +1,6 @@
 /*
  * zgetdump - Tool for copying and converting System z dumps
  *
- * Main include file - Should be included by all source files
- *
  * Copyright IBM Corp. 2001, 2018
  *
  * s390-tools is free software; you can redistribute it and/or modify
@@ -18,9 +16,11 @@
 /*
  * zgetdump globals
  */
-extern struct zgetdump_globals {
+struct zgetdump_globals {
 	struct zg_fh	*fh;
 	struct options	opts;
-} g;
+};
+
+extern struct zgetdump_globals g;
 
 #endif /* ZGETDUMP_H */
