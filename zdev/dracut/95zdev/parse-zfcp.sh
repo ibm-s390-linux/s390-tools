d370f7bae55259e6d027b4a576ead50e104a57d9
Steffen Maier <maier@linux.ibm.com>
2023-05-12 15:53:34 +0200

    zdev/dracut: produce helpful user information on parsing rd.zfcp
    It builds on the previously added helper function in parse-dasd.sh, which
    gets sourced first by dracut so the function is also available to the later
    parsing hook parse-zfcp.sh.
    
    Signed-off-by: Steffen Maier <maier@linux.ibm.com>

--- zdev/dracut/95zdev/parse-zfcp.sh
+++ zdev/dracut/95zdev/parse-zfcp.sh
@@ -10,7 +10,7 @@
 #   parameters are evaluated and used to configure zfcp devices.
 #
 
-zdev_zfcp_base_args="--no-settle --yes --quiet --no-root-update --force"
+zdev_zfcp_base_args="--no-settle --yes --no-root-update --force"
 
 for zdev_zfcp_arg in $(getargs rd.zfcp -d 'rd_ZFCP='); do
     (
@@ -20,14 +20,15 @@ for zdev_zfcp_arg in $(getargs rd.zfcp -d 'rd_ZFCP='); do
         set -- $zdev_zfcp_arg
         IFS=":" args="$*"
         IFS="$IFS_SAVED"
+        echo "rd.zfcp ${zdev_zfcp_arg} :" | zdev_vinfo
         if [ "$#" -eq 1 ]; then
             # shellcheck disable=SC2086
             chzdev --enable --persistent $zdev_zfcp_base_args \
-                   zfcp-host "$args"
+                   zfcp-host "$args" 2>&1 | zdev_vinfo
         else
             # shellcheck disable=SC2086
             chzdev --enable --persistent $zdev_zfcp_base_args \
-                   zfcp-lun "$args"
+                   zfcp-lun "$args" 2>&1 | zdev_vinfo
         fi
     )
 done
