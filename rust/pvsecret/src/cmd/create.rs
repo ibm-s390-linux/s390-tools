ibm-s390-linux/s390-tools

commit dd82c26f879d98e7e64540f5f9a1b8e4472aa600

rust: Add tool to manage UV-secrets
Add `pvsecret` a tool to create, add, list, and delete Ultravisor
secrets. `pvsecret` uses the functionality from the pv-crate
to provide an command line tool to manage the secrets.

Add a new target group PV_TARGETS in rust/Makefile that additionally
requires openssl and libcurl as pv with the feature "request" uses
openssl and libcurl fearures.

Acked-by: Jan Höppner <hoeppner@linux.ibm.com>
Acked-by: Marc Hartmayer <mhartmay@linux.ibm.com>
Signed-off-by: Steffen Eiden <seiden@linux.ibm.com>
[hoeppner@linux.ibm.com: Adapt man pages and help output]
Signed-off-by: Jan Höppner <hoeppner@linux.ibm.com>

rust/pvsecret/src/cmd/create.rs
@@ -1,0 +1,240 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright IBM Corp. 2023
+
+use crate::cli::{AddSecretType, CreateSecretFlags, CreateSecretOpt};
+use anyhow::{anyhow, bail, Context, Result};
+use log::{debug, info, trace, warn};
+use pv::{
+    misc::{
+        get_writer_from_cli_file_arg, parse_hex, pv_guest_bit_set, read_certs, read_exact_file,
+        read_file, try_parse_u128, try_parse_u64,
+    },
+    open_buffered_file,
+    request::{
+        openssl::pkey::{PKey, Public},
+        uvsecret::{AddSecretFlags, AddSecretRequest, AddSecretVersion, ExtSecret, GuestSecret},
+        BootHdrTags, HkdVerifier, ReqEncrCtx, Request, SymKeyType,
+    },
+    uv::ConfigUid,
+};
+use serde_yaml::Value;
+
+fn write_out<D: AsRef<[u8]>>(path: &str, data: D, ctx: &str) -> pv::Result<()> {
+    let mut wr = match get_writer_from_cli_file_arg(path) {
+        Ok(it) => it,
+        Err(err) => return Err(err),
+    };
+    pv::misc::write(&mut wr, data, path, ctx)?;
+    Ok(())
+}
+
+/// Prepare an Add Secret Request
+pub fn create(opt: &CreateSecretOpt) -> Result<()> {
+    if pv_guest_bit_set() {
+        warn!("The system seems to be a Secure Execution guest");
+        if !opt.force {
+            bail!("Do NOT generate Add-secret requests on a machine where you want to use the secret! Overwrite with '-f'");
+        } else {
+            warn!("WARNING: Enforcing of generating a request on a Secure Execution guest")
+        }
+    }
+
+    let mut asrcb = build_asrcb(opt)?;
+    debug!("Generated Add-secret request");
+
+    // Add host-key documents
+    let verifier = opt.certificate_args.verifier()?;
+    read_and_verify_hkds(&opt.certificate_args.host_key_documents, verifier)?
+        .into_iter()
+        .for_each(|k| asrcb.add_hostkey(k));
+
+    debug!("Added all host-keys");
+
+    // build + encrypt the request
+    let rq = ReqEncrCtx::random(SymKeyType::Aes256).context("Failed to generate random input")?;
+    let ser_asrbc = asrcb.encrypt(&rq)?;
+    warn!("Successfully generated the request");
+    write_out(&opt.output, ser_asrbc, "Add Secret Request")?;
+    info!("Successfully wrote the request to '{}'", &opt.output);
+
+    write_secret(&opt.secret, &asrcb)
+}
+
+/// Set-up the `Add Secret Request` from command-line arguments
+fn build_asrcb(opt: &CreateSecretOpt) -> Result<AddSecretRequest> {
+    debug!("Build Add Secret Request");
+
+    let secret = match &opt.secret {
+        AddSecretType::Meta => GuestSecret::Null,
+        AddSecretType::Association {
+            name,
+            input_secret: Some(p),
+            ..
+        } => GuestSecret::association(name, read_exact_file(p, "Association secret")?)?,
+        AddSecretType::Association {
+            name,
+            input_secret: None,
+            ..
+        } => GuestSecret::association(name, None)?,
+    };
+    trace!("AddSecret: {secret:x?}");
+
+    let mut flags = match &opt.pcf {
+        Some(v) => (&try_parse_u64(v, "pcf")?).into(),
+        None => AddSecretFlags::default(),
+    };
+    opt.flags.iter().for_each(|v| match v {
+        CreateSecretFlags::DisableDump => flags.set_disable_dump(),
+    });
+    debug!("FLAGS: {flags:x?}");
+
+    let mut se_hdr = open_buffered_file!(&opt.hdr);
+    let mut asrcb = AddSecretRequest::new(
+        AddSecretVersion::One,
+        secret,
+        BootHdrTags::from_se_image(&mut se_hdr)
+            .with_context(|| format!("Provided SE-header in '{}' is malformed", &opt.hdr))?,
+        flags,
+    );
+
+    // Set CUID
+    read_cuid(&mut asrcb, opt)?;
+
+    // Set extension secret
+    if let Some(path) = &opt.extension_secret {
+        asrcb.set_ext_secret(ExtSecret::Simple(
+            read_exact_file(path, "extension secret")?.into(),
+        ))?;
+    } else if let Some(path) = &opt.cck {
+        asrcb.set_ext_secret(ExtSecret::Derived(read_exact_file(path, "CCK")?.into()))?;
+    }
+
+    Ok(asrcb)
+}
+
+// Try to extract a Config-UId from a yaml structure
+// The cuid field can be embedded in an abritray amount of Mappings
+// The function takes the first cuid it founds (width search).
+fn try_from_val(val: Value) -> anyhow::Result<ConfigUid> {
+    fn get_cuid_from_mapping(val: &Value, depth: u8) -> Option<String> {
+        if depth >= 8 {
+            return None;
+        }
+        match val {
+            Value::Mapping(m) if m.contains_key("cuid") => {
+                return m.get("cuid").and_then(|v| v.as_str()).map(|s| s.to_owned())
+            }
+            Value::Mapping(m) => {
+                for (_, v) in m {
+                    if let Some(v) = get_cuid_from_mapping(v, depth + 1) {
+                        return Some(v);
+                    }
+                }
+            }
+            _ => return None,
+        };
+        None
+    }
+    let cuid = match &val {
+        Value::String(s) => Some(s.clone()),
+        Value::Mapping(_) => get_cuid_from_mapping(&val, 0),
+        _ => None,
+    }
+    .ok_or(anyhow!("No 'cuid' entry found"))?;
+    let cuid = cuid
+        .strip_prefix("0x")
+        .ok_or(anyhow!("Value starts not with 0x".to_string()))?
+        .to_owned();
+    if cuid.len() != ::std::mem::size_of::<ConfigUid>() * 2 {
+        return Err(anyhow!(format!("len invalid ({})", cuid.len())));
+    }
+    let cuid: ConfigUid = parse_hex(&cuid)
+        .try_into()
+        .map_err(|_| anyhow!("Cannot parse hex number".to_string()))?;
+    Ok(cuid)
+}
+
+fn read_cuid(asrcb: &mut AddSecretRequest, opt: &CreateSecretOpt) -> Result<()> {
+    if let Some(path) = &opt.cuid {
+        let cuid = match read_exact_file(path, "The CUID-file") {
+            Ok(v) => v,
+            Err(_) => {
+                let buf = read_file(path, "The CUID-file")?;
+                let val: Value = serde_yaml::from_slice(&buf).context(
+                    "The CUID-file does not contain a 128bit value or a yaml with a 'cuid' field",
+                )?;
+                try_from_val(val)?
+            }
+        };
+        asrcb.set_cuid(cuid);
+    } else if let Some(v) = &opt.cuid_hex {
+        asrcb.set_cuid(try_parse_u128(v, "CUID")?);
+    }
+    Ok(())
+}
+
+/// reads HKDs into memory, verifies them with the provided HKD verifier.
+/// returns list of public keys or Err
+/// Aborts on first error
+fn read_and_verify_hkds(
+    hkds: &Vec<String>,
+    verifier: Box<dyn HkdVerifier>,
+) -> Result<Vec<PKey<Public>>> {
+    let mut res = Vec::with_capacity(hkds.len());
+    for hkd in hkds {
+        let hk = read_file(hkd, "host-key document")?;
+        let certs = read_certs(&hk).with_context(|| {
+            format!("The provided Host Key Document in '{hkd}' is not in PEM or DER format")
+        })?;
+        if certs.len() != 1 {
+            warn!("The host key document in '{hkd}' contains more than one certificate!")
+        }
+
+        // len is 1 -> unwrap will succeed
+        let c = certs.first().unwrap();
+        verifier.verify(c)?;
+        res.push(c.public_key()?);
+        info!("Use host-key document at '{hkd}'");
+    }
+    Ok(res)
+}
+/// Write the generated secret (if any) to the specified output stream
+fn write_secret(secret: &AddSecretType, asrcb: &AddSecretRequest) -> Result<()> {
+    if let AddSecretType::Association {
+        name,
+        stdout,
+        output_secret: secret_out,
+        ..
+    } = secret
+    {
+        let gen_path: String = name
+            .chars()
+            .map(|c| if c.is_whitespace() { '_' } else { c })
+            .collect();
+
+        //write non confidential data (=name+id) to a yaml
+        let secret_info = serde_yaml::to_string(asrcb.guest_secret())?;
+        if stdout.to_owned() {
+            println!("{secret_info}");
+        } else {
+            let secret_info_path = format!("{gen_path}.yaml");
+            write_out(&secret_info_path, secret_info, "association secret info")?;
+            debug!(
+                "Non-confidential secret information: {:x?}",
+                asrcb.guest_secret()
+            );
+            warn!("Successfully wrote association info to '{secret_info_path}'");
+        }
+
+        if let Some(path) = secret_out {
+            if let GuestSecret::Association { secret, .. } = asrcb.guest_secret() {
+                write_out(path, secret.value(), "Association secret")?
+            } else {
+                unreachable!("The secret type has to be `association` at this point (bug)!")
+            }
+            info!("Successfully wrote generated association secret to '{path}'");
+        }
+    };
+    Ok(())
+}

rust/Makefile
@@ -1,10 +1,14 @@
 include ../common.mak
 SHELL := /bin/bash
 HAVE_CARGO ?= 1
+HAVE_OPENSSL ?= 1
+HAVE_LIBCURL ?= 1
 
 INSTALL_TARGETS := skip-build
 BUILD_TARGETS := skip-build
+PV_BUILD_TARGETS := skip-pv-build
 CARGO_TARGETS :=
+PV_TARGETS :=
 CARGO_TEST_TARGETS :=
 
 ifneq (${HAVE_CARGO},0)
@@ -14,7 +18,17 @@ ifneq (${HAVE_CARGO},0)
 	INSTALL_TARGETS := install-rust-tools install-man
 	CARGO_TEST_TARGETS = $(addsuffix, _test, $(CARGO_TARGETS))
 
-endif
+ifneq (${HAVE_OPENSSL},0)
+ifneq (${HAVE_LIBCURL},0)
+	PV_TARGETS := pvsecret
+
+	PV_BUILD_TARGETS = $(PV_TARGETS)
+	CARGO_TEST_TARGETS += $(addsuffix, _test, $(PV_TARGETS)) pv
+endif #LIBCURL
+endif #OPENSSL
+endif #CARGO
+
+BUILD_TARGETS += $(PV_BUILD_TARGETS)
 
 # build release targets by default
 ifeq ("${D}","0")
@@ -35,8 +49,15 @@ $(CARGO_TEST_TARGETS): .check-cargo .no-cross-compile
 	$(CARGO_TEST) --manifest-path=$@/Cargo.toml --all-features $(CARGOFLAGS)
 .PHONY: $(CARGO_TEST_TARGETS)
 
+$(PV_TARGETS): .check-cargo .no-cross-compile .check-dep-pvtools
+	$(CARGO_BUILD) --manifest-path=$@/Cargo.toml $(ALL_CARGOFLAGS)
+.PHONY: $(PV_TARGETS)
+
 skip-build:
 	echo "  SKIP    rust-tools due to unresolved dependencies"
+
+skip-pv-build:
+	echo "  SKIP    rust-pv-tools due to unresolved dependencies"
 
 all: $(BUILD_TARGETS)
 install: $(INSTALL_TARGETS)
@@ -47,6 +68,8 @@ print-rust-targets:
 clean:
 	$(foreach target,$(CARGO_TARGETS),\
 		$(CARGO_CLEAN) --manifest-path=$(target)/Cargo.toml ${ALL_CARGOFLAGS} ;)
+	$(foreach target,$(PV_TARGETS),\
+		$(CARGO_CLEAN) --manifest-path=$(target)/Cargo.toml ${CARGOFLAGS} ;)
 	$(RM) -- .check-dep-pvtools .detect-openssl.dep.c .check-cargo
 
 rust-test: .check-cargo .no-cross-compile
@@ -57,9 +80,13 @@ install-rust-tools: $(BUILD_TARGETS)
 	$(INSTALL) -d -m 755 $(DESTDIR)$(USRBINDIR)
 	$(foreach target,$(CARGO_TARGETS),\
 		$(INSTALL) $(target)/target/release/$(target) $(DESTDIR)$(USRBINDIR);)
+	$(foreach target,$(PV_TARGETS),\
+		$(INSTALL) $(target)/target/release/$(target) $(DESTDIR)$(USRBINDIR);)
 
 install-man:
 	$(foreach target,$(CARGO_TARGETS),\
+		$(INSTALL) -m 644 $(target)/man/*.1 -t $(DESTDIR)$(MANDIR)/man1;)
+	$(foreach target,$(PV_TARGETS),\
 		$(INSTALL) -m 644 $(target)/man/*.1 -t $(DESTDIR)$(MANDIR)/man1;)
 
 .PHONY: all install clean skip-build install-rust-tools print-rust-targets install-man rust-test
@@ -79,3 +106,27 @@ ifneq ($(HOST_ARCH), $(BUILD_ARCH))
 	$(error Cross compiling is not supported for rust code. Specify HAVE_CARGO=0 to disable rust compilation)
 endif
 .PHONY: .no-cross-compile
+
+.detect-openssl.dep.c:
+	echo "#include <openssl/evp.h>" > $@
+	echo "#if OPENSSL_VERSION_NUMBER < 0x10101000L" >> $@
+	echo "  #error openssl version 1.1.1 is required" >> $@
+	echo "#endif" >> $@
+	echo "static void __attribute__((unused)) test(void) {" >> $@
+	echo "    EVP_MD_CTX *ctx = EVP_MD_CTX_new();" >> $@
+	echo "    EVP_MD_CTX_free(ctx);" >> $@
+	echo "}" >> $@
+
+.check-dep-pvtools: .detect-openssl.dep.c
+	$(call check_dep, \
+		"$(BIN_PROGRAM)", \
+		$^, \
+		"openssl-devel / libssl-dev version >= 1.1.1", \
+		"HAVE_OPENSSL=0", \
+		"-I.")
+	$(call check_dep, \
+		"$(BIN_PROGRAM)", \
+		"curl/curl.h", \
+		"libcurl-devel", \
+		"HAVE_LIBCURL=0")
+	touch $@

rust/pvsecret/.gitignore
@@ -1,0 +1,1 @@
+!Cargo.lock

rust/pvsecret/Cargo.toml
@@ -1,0 +1,18 @@
+[package]
+name = "pvsecret"
+version = "0.9.0"
+edition = "2021"
+license = "MIT"
+
+[dependencies]
+anyhow = { version = "1", features = ["std"] }
+clap = { version ="4", features = ["derive", "wrap_help"]}
+log = { version = "0.4", features = ["std", "release_max_level_debug"] }
+serde_yaml = "0.9"
+
+pv = { path = "../pv", features = ["uvsecret", "request"] }
+utils = { path = "../utils" }
+
+[profile.release]
+lto = true
+panic = "abort" # release builds now do not clean up stack after panics. .1 Mb

rust/pvsecret/man/pvsecret-add.1
@@ -1,0 +1,33 @@
+.\" Copyright 2023 IBM Corp.
+.\" s390-tools is free software; you can redistribute it and/or modify
+.\" it under the terms of the MIT license. See LICENSE for details.
+.\"
+
+.TH pvsecret-add 1 "2023-07-28" "s390-tools" "UV-Secret Manual"
+.nh
+.ad l
+.SH NAME
+\fBpvsecret add\fP - Repeat an add-secret request (s390x only)
+\fB
+.SH SYNOPSIS
+.nf
+.fam C
+pvsecret add <FILE>
+.fam C
+.fi
+.SH DESCRIPTION
+Perform an add-secret request using a previously generated add-secret request.
+Only available on s390x.
+
+.SH OPTIONS
+.PP
+<FILE>
+.RS 4
+Specify the request to be sent.
+.RE
+.RE
+
+
+.SH "SEE ALSO"
+.sp
+\fBpvsecret\fR(1)

rust/pvsecret/man/pvsecret-create-association.1
@@ -1,0 +1,56 @@
+.\" Copyright 2023 IBM Corp.
+.\" s390-tools is free software; you can redistribute it and/or modify
+.\" it under the terms of the MIT license. See LICENSE for details.
+.\"
+
+.TH pvsecret-create-association 1 "2023-07-28" "s390-tools" "UV-Secret Manual"
+.nh
+.ad l
+.SH NAME
+\fBpvsecret create association\fP - Use an association secret to connect an I/O device to a guest
+\fB
+.SH SYNOPSIS
+.nf
+.fam C
+pvsecret create association [OPTIONS] <NAME>
+.fam C
+.fi
+.SH DESCRIPTION
+Use an association secret to connect a trusted I/O device to a guest. The
+`pvapconfig` tool provides more information about association secrets.
+
+.SH OPTIONS
+.PP
+<NAME>
+.RS 4
+String to identify the new secret. The actual secret is set with
+\fB--input-secret\fR.  The name is saved in `NAME.yaml` with white-spaces mapped
+to `_`.
+.RE
+.RE
+
+.PP
+\-\-stdout
+.RS 4
+Print the hashed name to stdout. The hashed name is not written to `NAME.yaml`
+.RE
+.RE
+.PP
+\-\-input-secret <FILE>
+.RS 4
+Path from which to read the plaintext secret. Uses a random secret if not
+specified.
+.RE
+.RE
+.PP
+\-\-output-secret <FILE>
+.RS 4
+Save the generated secret as plaintext in FILE. The generated secret can be used
+to generate add-secret requests for a different guest with the same secret using
+\fB--input-secret\fR. Destroy the secret when it is not used anymore.
+.RE
+.RE
+
+.SH "SEE ALSO"
+.sp
+\fBpvsecret\fR(1) \fBpvsecret-create\fR(1)

rust/pvsecret/man/pvsecret-create-meta.1
@@ -1,0 +1,24 @@
+.\" Copyright 2023 IBM Corp.
+.\" s390-tools is free software; you can redistribute it and/or modify
+.\" it under the terms of the MIT license. See LICENSE for details.
+.\"
+
+.TH pvsecret-create-meta 1 "2023-07-28" "s390-tools" "UV-Secret Manual"
+.nh
+.ad l
+.SH NAME
+\fBpvsecret create meta\fP - Use a meta secret to carry flags to the ultravisor
+\fB
+.SH SYNOPSIS
+.nf
+.fam C
+pvsecret create meta
+.fam C
+.fi
+.SH DESCRIPTION
+Use a meta secret to carry flags to the ultravisor without having to provide an
+actual secret value.  Meta secrets do not appear in the list of secrets.
+
+.SH "SEE ALSO"
+.sp
+\fBpvsecret\fR(1) \fBpvsecret-create\fR(1)

rust/pvsecret/man/pvsecret-create.1
@@ -1,0 +1,154 @@
+.\" Copyright 2023 IBM Corp.
+.\" s390-tools is free software; you can redistribute it and/or modify
+.\" it under the terms of the MIT license. See LICENSE for details.
+.\"
+
+.TH pvsecret-create 1 "2023-07-28" "s390-tools" "UV-Secret Manual"
+.nh
+.ad l
+.SH NAME
+\fBpvsecret create\fP - Create a new add-secret request
+\fB
+.SH SYNOPSIS
+.nf
+.fam C
+pvsecret create [OPTIONS] --host-key-document <FILE> --hdr <FILE> --output <FILE> <--no-verify|--cert <FILE>> <COMMAND>
+.fam C
+.fi
+.SH DESCRIPTION
+Create add-secret requests for IBM Secure Execution guests. Only create these
+requests in a trusted environment, such as your workstation. The \fBpvattest
+create\fR command creates a randomly generated key to protect the request. The
+generated requests can then be added on an IBM Secure Execution guest using
+\fBpvsecret add\fR. The guest can then use the secrets with the use case
+depending on the secret type.
+
+Such a request is bound to a specific IBM Secure Execution image specified with
+\fB--hdr\fR. Optionally, the request can be bound to a specific instance when
+bound to the Configuration Unique ID from \fBpvattest\fR using \fB--cuid\fR
+.SH OPTIONS
+.PP
+\-k, \-\-host-key-document <FILE>
+.RS 4
+Use FILE as a host-key document. Can be specified multiple times and must be
+used at least once.
+.RE
+.RE
+.PP
+\-\-no-verify
+.RS 4
+Disable the host-key document verification. Does not require the host-key
+documents to be valid. Do not use for a production request unless you verified
+the host-key document beforehand.
+.RE
+.RE
+.PP
+\-C, \-\-cert <FILE>
+.RS 4
+Use FILE as a certificate to verify the host key or keys. The certificates are
+used to establish a chain of trust for the verification of the host-key
+documents. Specify this option twice to specify the IBM Z signing key and the
+intermediate CA certificate (signed by the root CA).
+.RE
+.RE
+.PP
+\-\-crl <FILE>
+.RS 4
+Use FILE as a certificate revocation list. The list is used to check whether a
+certificate of the chain of trust is revoked. Specify this option multiple times
+to use multiple CRLs.
+.RE
+.RE
+.PP
+\-\-offline
+.RS 4
+Make no attempt to download CRLs
+.RE
+.RE
+.PP
+\-\-root-ca <ROOT_CA>
+.RS 4
+Use FILE as the root-CA certificate for the verification. If omitted, the
+system-wide root CAs installed on the system are used. Use this only if you
+trust the specified certificate.
+.RE
+.RE
+.PP
+\-\-hdr <FILE>
+.RS 4
+Specifies the header of the guest image. Can be an IBM Secure Execution image
+created by genprotimg or an extracted IBM Secure Execution header. The header
+must start at a page boundary.
+.RE
+.RE
+.PP
+\-f, \-\-force
+.RS 4
+Force the generation of add-secret requests on IBM Secure Execution guests. If
+the program detects that it is running on an IBM Secure Execution guest, it
+denies the generation of add-secret requests. The force flag overwrites this
+behavior.
+.RE
+.RE
+.PP
+\-o, \-\-output <FILE>
+.RS 4
+Write the generated request to FILE
+.RE
+.RE
+.PP
+\-\-extension-secret <FILE>
+.RS 4
+Use the content of FILE as an extension secret. The file must be exactly 32
+bytes long. If this request is the first, all subsequent requests must have the
+same extension secret. Only makes sense if bit 1 of the secret control flags of
+the IBM Secure Execution header is 0. Otherwise the ultravisor rejects the
+request.
+.RE
+.RE
+.PP
+\-\-cck <FILE>
+.RS 4
+Use the content of FILE as the customer-communication key (CCK) to derive the
+extension secret. The file must contain exactly 32 bytes of data. If the target
+guest was started with bit 1 of the secret control flag set, the ultravisor also
+derives the secret from the CCK. Otherwise, the ultravisor interprets the
+extension secret as a normal one. This still works if you use the same CCK for
+all requests.
+.RE
+.RE
+.PP
+\-\-cuid-hex <HEXSTRING>
+.RS 4
+Use HEXSTRING as the Configuration Unique ID. Must be a hex 128-bit unsigned
+big endian number string. Leading zeros must be provided. If specified, the
+value must match with the Config-UID from the attestation result of that guest.
+If not specified, the CUID will be ignored by the ultravisor during the
+verification of the request.
+.RE
+.RE
+.PP
+\-\-cuid <FILE>
+.RS 4
+Use the content of FILE as the Configuration Unique ID. The file must contain
+exactly 128 bit of data, a hex string, or a yaml with a `cuid` entry. If
+specified, the value must match the Config-UID from the attestation result of
+that guest. If not specified, the CUID will be ignored by the Ultravisor during
+the verification of the request.
+.RE
+.RE
+.PP
+\-\-flags <FLAGS>
+.RS 4
+Flags for the add-secret request.
+
+Possible values:
+.RS 4
+- \fBdisable-dump\fP: Disables host-initiated dumping for the target guest instance.
+
+.RE
+.RE
+
+.SH "SEE ALSO"
+.sp
+\fBpvsecret\fR(1) \fBpvsecret-create-meta\fR(1) \fBpvsecret-create-association\fR(1)

commit 25aac53d52b9762d8abd72248ed5a1092e8d5664

rust/pvsecret: Fix secret info file location
Write the secret metadata into the same directory as the generated
secret request. Before, the metadata was accidentally written into the
working directory.

Fixes: dd82c26f879d ("rust: Add tool to manage UV-secrets")
Suggested-by: Marc Hartmayer <mhartmay@linux.ibm.com>
Reviewed-by: Marc Hartmayer <mhartmay@linux.ibm.com>
Signed-off-by: Steffen Eiden <seiden@linux.ibm.com>

rust/pvsecret/src/cmd/create.rs
@@ -2,6 +2,8 @@
 //
 // Copyright IBM Corp. 2023, 2024
 
+use std::path::Path;
+
 use crate::cli::{AddSecretType, CreateSecretFlags, CreateSecretOpt};
 use anyhow::{anyhow, bail, Context, Error, Result};
 use log::{debug, info, trace, warn};
@@ -20,8 +22,12 @@ use pv::{
 use serde_yaml::Value;
 use utils::get_writer_from_cli_file_arg;
 
-fn write_out<D: AsRef<[u8]>>(path: &str, data: D, ctx: &str) -> pv::Result<()> {
-    let mut wr = get_writer_from_cli_file_arg(path)?;
+fn write_out<P, D>(path: &P, data: D, ctx: &str) -> pv::Result<()>
+where
+    P: AsRef<Path>,
+    D: AsRef<[u8]>,
+{
+    let mut wr = get_writer_from_cli_file_arg(path.as_ref())?;
     write(&mut wr, data, path, ctx)?;
     Ok(())
 }
@@ -55,7 +61,7 @@ pub fn create(opt: &CreateSecretOpt) -> Result<()> {
     write_out(&opt.output, ser_asrbc, "add-secret request")?;
     info!("Successfully wrote the request to '{}'", &opt.output);
 
-    write_secret(&opt.secret, &asrcb)
+    write_secret(&opt.secret, &asrcb, &opt.output)
 }
 
 /// Read+parse the first key from the buffer.
@@ -200,7 +206,11 @@ fn read_cuid(asrcb: &mut AddSecretRequest, opt: &CreateSecretOpt) -> Result<()> 
 }
 
 /// Write the generated secret (if any) to the specified output stream
-fn write_secret(secret: &AddSecretType, asrcb: &AddSecretRequest) -> Result<()> {
+fn write_secret<P: AsRef<Path>>(
+    secret: &AddSecretType,
+    asrcb: &AddSecretRequest,
+    outp_path: P,
+) -> Result<()> {
     if let AddSecretType::Association {
         name,
         stdout,
@@ -208,23 +218,31 @@ fn write_secret(secret: &AddSecretType, asrcb: &AddSecretRequest) -> Result<()> 
         ..
     } = secret
     {
-        let gen_path: String = name
+        let gen_name: String = name
             .chars()
             .map(|c| if c.is_whitespace() { '_' } else { c })
             .collect();
+        let mut gen_path = outp_path
+            .as_ref()
+            .parent()
+            .with_context(|| format!("Cannot open directory of {:?}", outp_path.as_ref()))?
+            .to_owned();
+        gen_path.push(format!("{gen_name}.yaml"));
 
         // write non confidential data (=name+id) to a yaml
         let secret_info = serde_yaml::to_string(asrcb.guest_secret())?;
         if stdout.to_owned() {
             println!("{secret_info}");
         } else {
-            let secret_info_path = format!("{gen_path}.yaml");
-            write_out(&secret_info_path, secret_info, "association secret info")?;
+            write_out(&gen_path, secret_info, "association secret info")?;
             debug!(
                 "Non-confidential secret information: {:x?}",
                 asrcb.guest_secret()
             );
-            warn!("Successfully wrote association info to '{secret_info_path}'");
+            warn!(
+                "Successfully wrote association info to '{}'",
+                gen_path.display()
+            );
         }
 
         if let Some(path) = secret_out {