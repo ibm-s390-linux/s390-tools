8dfeb0565d17512d937fcc64c5cc3d3f3e045e86
Vojtech Trefny <vtrefny@redhat.com>
2025-12-03 14:47:34 +0100

    fdasd: Add non-interactive script friendly interface
    This is a work-in-progress implementation of scripting interface
    similar to how 'parted --script' works, for now limited to adding
    and removing partitions only.
    
    Signed-off-by: Vojtech Trefny <vtrefny@redhat.com>

--- fdasd/fdasd.c
+++ fdasd/fdasd.c
@@ -9,6 +9,7 @@
 
 #include <getopt.h>
 #include <stdio.h>
+#include <strings.h>
 #include <sys/sysmacros.h>
 
 #include "lib/dasd_base.h"
@@ -156,6 +157,11 @@ static struct util_opt opt_vec[] = {
 		.option = { "label", required_argument, NULL, 'l' },
 		.argument = "VOLSER",
 		.desc = "Set the volume serial to VOLSER",
+	},
+	{
+		.option = { "script", required_argument, NULL, 'S' },
+		.argument = "COMMANDS",
+		.desc = "Execute commands in non-interactive mode",
 	},
 	UTIL_OPT_SECTION("MISC"),
 	{
@@ -636,6 +642,11 @@ static void fdasd_parse_options(fdasd_anchor_t *anc,
 		case 'a':
 			anc->auto_partition++;
 			break;
+		case 'S':
+			anc->script_mode++;
+			anc->silent++;
+			options->script_commands = optarg;
+			break;
 		case 's':
 			anc->silent++;
 			break;
@@ -1039,6 +1050,12 @@ static void fdasd_verify_options(fdasd_anchor_t *anc)
 		fdasd_error(anc, parser_failed,
 			    "Option 'config' cannot be used with"
 			    " 'table'.\n");
+	}
+
+	if (anc->script_mode && (anc->auto_partition || options.conffile)) {
+		fdasd_error(anc, parser_failed,
+			    "Option 'script' cannot be used with"
+			    " 'auto' or 'config'.\n");
 	}
 }
 
@@ -2978,6 +2995,271 @@ static void fdasd_quit(fdasd_anchor_t *anc)
 	fdasd_exit(anc, 0);
 }
 
+static int fdasd_remove_partition_script(fdasd_anchor_t *anc,
+					 unsigned int part_id)
+{
+	int rc;
+
+	rc = fdasd_commit_remove_partition(anc, part_id);
+	if (rc < 0) {
+		printf("Invalid partition id %d\n", part_id);
+		return -1;
+	}
+
+	return 0;
+}
+
+static int fdasd_get_partition_data_script(fdasd_anchor_t *anc,
+					   extent_t *part_extent,
+					   partition_info_t *part_info,
+					   unsigned long start,
+					   unsigned long stop)
+{
+	partition_info_t *part_tmp;
+	cchh_t llimit, ulimit;
+	u_int16_t hh, head;
+	u_int32_t cc, cyl;
+	u_int8_t b1, b2;
+	unsigned long limit;
+
+	cyl = get_usable_cylinders(anc);
+	head = anc->f4->DS4DEVCT.DS4DSTRK;
+	limit = (head * cyl - 1);
+
+	if (start < FIRST_USABLE_TRK) {
+		printf("Error: Start track %ld is below minimum (%d)\n",
+		       start, FIRST_USABLE_TRK);
+		return -1;
+	}
+
+	if (start > limit) {
+		printf("Error: Start track %ld exceeds disk limit (%ld)\n",
+		       start, limit);
+		return -1;
+	}
+
+	if (stop < start) {
+		printf("Error: Stop track %ld is before start track %ld\n",
+		       stop, start);
+		return -1;
+	}
+
+	if (stop > limit) {
+		printf("Error: Stop track %ld exceeds disk limit (%ld)\n",
+		       stop, limit);
+		return -1;
+	}
+
+	/* check for overlaps with existing partitions */
+	for (part_tmp = anc->first; part_tmp->next; part_tmp = part_tmp->next) {
+		/* Check if start is within an existing partition */
+		if (start >= part_tmp->start_trk && start <= part_tmp->end_trk) {
+			printf("Error: Start track %ld overlaps with partition %ld-%ld\n",
+			       start, part_tmp->start_trk, part_tmp->end_trk);
+			return -1;
+		}
+
+		if (stop >= part_tmp->start_trk && stop <= part_tmp->end_trk) {
+			printf("Error: Stop track %ld overlaps with partition %ld-%ld\n",
+			       stop, part_tmp->start_trk, part_tmp->end_trk);
+			return -1;
+		}
+
+		if (start < part_tmp->start_trk && stop > part_tmp->end_trk) {
+			printf("Error: New partition would encompass existing partition %ld-%ld\n",
+			       part_tmp->start_trk, part_tmp->end_trk);
+			return -1;
+		}
+	}
+
+	/* update partition info */
+	part_info->len_trk   = stop - start + 1;
+	part_info->start_trk = start;
+	part_info->end_trk   = stop;
+
+	cc = start / geo.heads;
+	hh = start - (cc * geo.heads);
+	vtoc_set_cchh(&llimit, cc, hh);
+
+	/* check for cylinder boundary */
+	if (hh == 0)
+		b1 = 0x81;
+	else
+		b1 = 0x01;
+
+	cc = stop / geo.heads;
+	hh = stop - cc * geo.heads;
+	vtoc_set_cchh(&ulimit, cc, hh);
+
+	/* it is always the 1st extent */
+	b2 = 0x00;
+
+	vtoc_set_extent(part_extent, b1, b2, &llimit, &ulimit);
+
+	return 0;
+}
+
+static int fdasd_add_partition_script(fdasd_anchor_t *anc,
+				      unsigned long start_trk,
+				      unsigned long stop_trk)
+{
+	partition_info_t *part_info;
+	extent_t ext;
+	int rc;
+
+	part_info = fdasd_get_empty_f1_label(anc);
+	if (!part_info) {
+		printf("No more free partitions left,\n"
+		       "you have to delete one first!");
+		return -1;
+	}
+
+	rc = fdasd_get_partition_data_script(anc, &ext, part_info,
+					     start_trk, stop_trk);
+	if (rc != 0)
+		return -1;
+
+	fdasd_commit_new_partition(anc, part_info, &ext);
+
+	return 0;
+}
+
+static int fdasd_change_part_type_script(fdasd_anchor_t *anc, unsigned int part_id, unsigned int part_type)
+{
+	partition_info_t *part_info;
+	char *dsname = NULL;
+	char str[20], *ch;
+	unsigned int i = 0;
+
+	if (part_id == 0 || part_id > anc->used_partitions) {
+		printf("'%d' is not a valid partition id!\n", part_id);
+		return -1;
+	}
+
+	part_info = anc->first;
+	for (i = 1; i < part_id; i++)
+		part_info = part_info->next;
+
+	vtoc_ebcdic_dec(part_info->f1->DS1DSNAM, part_info->f1->DS1DSNAM, 44);
+	ch = strstr(part_info->f1->DS1DSNAM, "PART") + 9;
+	if (ch) {
+		strncpy(str, ch, 6);
+		str[6] = '\0';
+	} else {
+		strcpy(str, "error");
+	}
+
+	if (get_part_dsname_by_type(part_type, &dsname)) {
+		printf("part type '%d' is not supported!\n", part_type);
+		return -1;
+	}
+
+	snprintf(str, 7, "%-6s", dsname);
+	ch = strstr(part_info->f1->DS1DSNAM, "PART") + 9;
+	if (ch)
+		memcpy(ch, str, 6);
+	vtoc_ebcdic_enc(part_info->f1->DS1DSNAM, part_info->f1->DS1DSNAM, 44);
+	anc->vtoc_changed++;
+
+	return 0;
+}
+
+static char *_skip_whitespace(char *str)
+{
+	while (*str && isspace(*str))
+		str++;
+	return str;
+}
+
+static void fdasd_process_script_commands(fdasd_anchor_t *anc, char *commands)
+{
+	char *ptr = commands;
+	int rc = 0;
+	char cmd;
+
+	while (*ptr) {
+		ptr = _skip_whitespace(ptr);
+
+		if (*ptr == '\0')
+			break;
+
+		cmd = *ptr++;
+		if (cmd == 'n') {
+			unsigned long long start_trk, stop_trk;
+
+			ptr = _skip_whitespace(ptr);
+
+			/* start track */
+			start_trk = atoi(ptr++);
+			while (isdigit(*ptr))
+				ptr++;
+			if (start_trk == 0) {
+				printf("Invalid start position\n");
+				fdasd_exit(anc, EXIT_FAILURE);
+			}
+
+			ptr = _skip_whitespace(ptr);
+
+			/* stop track (can be relative to start) */
+			if (*ptr == '+') {
+				++ptr;
+				line_ptr = ptr;
+				if (fdasd_parse_relative_size(&stop_trk, anc) != 0) {
+					printf("Invalid stop position\n");
+					fdasd_exit(anc, EXIT_FAILURE);
+				}
+				ptr = ++line_ptr;
+				stop_trk += (start_trk - 1);
+			} else {
+				stop_trk = atoi(ptr++);
+				while (isdigit(*ptr))
+					ptr++;
+			}
+
+			rc = fdasd_add_partition_script(anc, start_trk, stop_trk);
+			if (rc < 0)
+				fdasd_exit(anc, EXIT_FAILURE);
+		} else if (cmd == 'd') {
+			unsigned int part_id;
+
+			part_id = atoi(ptr++);
+			while (isdigit(*ptr))
+				ptr++;
+
+			rc = fdasd_remove_partition_script(anc, part_id);
+			if (rc < 0)
+				fdasd_exit(anc, EXIT_FAILURE);
+		} else if (cmd == 't') {
+			unsigned int part_id, part_type;
+
+			part_id = atoi(ptr++);
+			while (isdigit(*ptr))
+				ptr++;
+			ptr = _skip_whitespace(ptr);
+
+			part_type = atoi(ptr++);
+			while (isdigit(*ptr))
+				ptr++;
+
+			rc = fdasd_change_part_type_script(anc, part_id, part_type);
+			if (rc < 0)
+				fdasd_exit(anc, EXIT_FAILURE);
+		} else if (cmd == 'r') {
+			fdasd_recreate_vtoc_unconditional (anc);
+		} else if (cmd == 'q') {
+			fdasd_exit(anc, EXIT_SUCCESS);
+		} else if (cmd == 'w') {
+			fdasd_write_labels(anc);
+			fdasd_exit(anc, EXIT_SUCCESS);
+		} else {
+			printf("Unknown command '%c'\n", cmd);
+			fdasd_exit(anc, EXIT_FAILURE);
+		}
+	}
+
+	fdasd_exit(anc, EXIT_SUCCESS);
+}
+
 /*
  *
  */
@@ -3022,6 +3304,9 @@ int main(int argc, char *argv[])
 			fdasd_list_partition_table(&anchor);
 		fdasd_quit(&anchor);
 	}
+
+	if (anchor.script_mode)
+		fdasd_process_script_commands(&anchor, options.script_commands);
 
 	fdasd_menu();
 
