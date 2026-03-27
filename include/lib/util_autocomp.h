8c99c3878ef48a684afd3184e2a9d1839c5ea886
Szabina Korbai <szkorbai@linux.ibm.com>
2026-03-27 10:30:53 +0000

    libutil/util_autocomp_host: Remove license boilerplate
    Remove outdated license boilerplate and fix
    SPDX-tag style.
    
    Acked-by: Steffen Eiden <seiden@linux.ibm.com>
    Reviewed-by: Jan Höppner <hoeppner@linux.ibm.com>
    Signed-off-by: Szabina Korbai <szkorbai@linux.ibm.com>
    Signed-off-by: Jan Höppner <hoeppner@linux.ibm.com>

--- include/lib/util_autocomp.h
+++ include/lib/util_autocomp.h
@@ -1,15 +1,7 @@
-/* SPDX-License-Identifier: MIT */
 /*
- * autocomp - command line autocompletion
+ * SPDX-License-Identifier: MIT
  *
- * Generating autocompletion scripts for bash and zsh
- * based on util_opt struct
- *
- * Copyright IBM Corp. 2025
- *
- * s390-tools is free software; you can redistribute it and/or modify
- * it under the terms of the MIT license. See LICENSE for details.
- *
+ * Copyright IBM Corp.
  */
 
 #ifndef LIB_UTIL_AUTOCOMP_H
